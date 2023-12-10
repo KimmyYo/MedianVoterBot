a3f1109d399379d7cb2515a45a9f231682e8215e
KimmyYo <113627877+KimmyYo@users.noreply.github.com>
2023-12-10 13:20:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -195,4 +195,5 @@ dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_user_input
 
 
 if __name__ == '__main__':
-   app.run(debug=True)
+   port = int(os.environ.get("PORT", 5000))
+   app.run(host="0.0.0.0", port=port)
